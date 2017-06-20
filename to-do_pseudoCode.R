f87226ab38e5ac7fa55b89518a389995e2b0394f
ajrominger <ajrominger@gmail.com>
2017-06-20 09:59:12 -0600

    pseudo code for how to manage to-do list

---
+++ to-do_pseudoCode.R
@@ -0,0 +1,10 @@
+## pull previous commit from git
+## find changes
+    ## if changes are change to header name, do nothing
+    ## if changes are addition, do nothing
+    ## if changes are deleted item, add back deleted item, but format as crossed off
+        ## check that deleted items aren't actually just re-arrangement
+        ## check that deleted items aren't minor edits
+        ## might be easiest to have a commit function for edits vs. accomplishments
+## run pandoc to format new document
+## commit it back to git
